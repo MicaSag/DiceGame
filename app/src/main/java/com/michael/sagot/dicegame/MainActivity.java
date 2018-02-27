5ec8edee1905ae2e145f3666eccc228626a97930
Michaël <sagotmickael@neuf.fr>
2018-02-28 00:07:56 +0100

    _ Implementation of the controllers

--- app/src/main/java/com/michael/sagot/dicegame/MainActivity.java
+++ app/src/main/java/com/michael/sagot/dicegame/MainActivity.java
@@ -1,13 +1,47 @@
 package com.michael.sagot.dicegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button mButton6;  // Button for 6 sided dice choice
+    protected Button mButton20; // Button for 20 sides dice choice
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // >> Management of the Button for 6 sided dice choice <<
+        mButton6 = findViewById(R.id.activity_main_6dices_btn);
+        // Encoding the Listener of the button
+        mButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // Action during a click on the button
+            public void onClick(View view) {
+                // Create a Intent for call RollActivity
+                Intent intent = new Intent(MainActivity.this,RollActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // >> Management of the Button for 20 sided dice choice <<
+        mButton20 = findViewById(R.id.activity_main_20dices_btn);
+        // Encoding the Listener of the button
+        mButton20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a Intent for call RollActivity
+                Intent intent = new Intent(MainActivity.this,RollActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
--- app/src/main/java/com/michael/sagot/dicegame/RollActivity.java
+++ app/src/main/java/com/michael/sagot/dicegame/RollActivity.java
@@ -1,13 +1,46 @@
 package com.michael.sagot.dicegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.security.SecureRandom;
 
 public class RollActivity extends AppCompatActivity {
+
+    protected Button mButtonRollDice;
+    protected TextView mTitleDice;
+    protected TextView mResultDice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roll);
+
+        // >> Management of the TextView of the dice title <<
+        mTitleDice = findViewById(R.id.activity_roll_diceTitle_txt);
+        mTitleDice.setText("6 sided dice");
+
+        // >> Management of the TextView of the dice result <<
+        mResultDice = findViewById(R.id.activity_roll_diceResult_txt);
+        mResultDice.setText("");
+
+        // >> Management of the Button for Roll the dice <<
+        mButtonRollDice = findViewById(R.id.activity_roll_buttonRoll_btn);
+        // Encoding the Listener of the button
+        mButtonRollDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // Action during a click on the button
+            public void onClick(View view) {
+                // Acquisition of a random value between 1 and 6
+                SecureRandom random = new SecureRandom();
+                mResultDice.setText(String.valueOf(random.nextInt(6) + 1));
+            }
+        });
     }
 }
